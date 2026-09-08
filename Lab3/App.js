799daf62840f3bcd0a39018806583e6448462340
SHRED <shred@SHREDs-MacBook-Pro.local>
2026-09-08 10:09:10 +0530

    add helper function

--- Lab3/App.js
+++ Lab3/App.js
@@ -2,7 +2,26 @@ import http from "http";
 import { getAllTeams } from "./pages/teams.js";
 
 const PORT = 6000;
-
+const sendJSON=(res,statusCode,data)=>{
+    res.writeHead(statusCode,{"content-type":"application/json"});
+    res.end(data === "undefined" ? "":JSON.stringify(data));
+}
+const parseBody =(req)=>{
+    new Promise((resolve,reject)=>{
+        let body = "";
+        req.on("data",(chunk)=>{
+            body +=chunk.toString();
+        });
+        req.on("end",()=>{
+            try{
+                resolve(body ? JSON.parse(body):{});
+            }
+            catch (error){
+                reject(error);
+            }
+        });
+    });
+};
 
 const server = http.createServer((req, res) => {
     if (req.url === "/" && req.method === "GET") {
